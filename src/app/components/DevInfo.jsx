573895e66bbec67456e217ddc873eec5ca544fb4
Franklin Gomez <franklingomezdev@gmail.com>
2025-04-02 10:46:20 -0400

    created structure for dev component. fixed some CSS.

---
+++ src/app/components/DevInfo.jsx
@@ -0,0 +1,16 @@
+
+export default function DevInfo() {
+    return (
+        <div className="dev-info">
+            <div className="avatar-container">
+                <img src="" alt="" />
+            </div>
+            <div className="info-container">
+                <div className="info-container-header"></div>
+                <div className="info-container-bio"></div>
+                <div className="info-container-stats"></div>
+                <div className="info-container-links"></div>
+            </div>
+        </div>
+    )
+}
